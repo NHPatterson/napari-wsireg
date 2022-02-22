2d3ef18fc2c7488994e6c950106c254104f047cb
nhp <nheathpatterson@gmail.com>
2022-02-22 09:05:43 -0600

    improve file dialog title and file types

--- src/napari_wsireg/gui/utils/file.py
+++ src/napari_wsireg/gui/utils/file.py
@@ -10,9 +10,32 @@ def open_file_dialog(
     parent_widg: QWidget,
     single: bool = True,
     wd: PathLike = "",
-    name: str = "Open files",
-    file_types: str = "All Files (*)",
+    data_type: str = "image",
+    file_types: Optional[str] = "All Files (*)",
 ) -> Optional[PathLike]:
+
+    if data_type == "image":
+        name = "Open registration image(s)..."
+        file_types = (
+            "images (*.tiff *.tif *.svs *.ndpi *.scn *.czi);;"
+            "Zeiss Czi (*.czi);;All Files (*)"
+        )
+
+    elif data_type == "attachment":
+        name = "Open attachment image..."
+        file_types = (
+            "tifffile images (*.tiff *.tif *.svs *.ndpi *.scn *.czi);;"
+            "Zeiss Czi (*.czi);;All Files (*)"
+        )
+
+    elif data_type == "shape":
+        name = "Open attachment shape data..."
+        file_types = "geojson files (*.geojson *.json);;All Files (*)"
+
+    else:
+        name = "Open file(s)..."
+        file_types = file_types
+
     if single is False:
         file_path, _ = QFileDialog.getOpenFileNames(
             parent_widg,
