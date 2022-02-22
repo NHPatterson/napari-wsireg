c08f086d3d3e6db9ae31289784890582ca41c70b
nhp <nheathpatterson@gmail.com>
2022-02-22 09:10:36 -0600

    add run options for graph

--- src/napari_wsireg/gui/setup_sub/project.py
+++ src/napari_wsireg/gui/setup_sub/project.py
@@ -5,7 +5,9 @@ from qtpy.QtWidgets import (
     QPushButton,
     QVBoxLayout,
     QWidget,
+    QCheckBox,
 )
+from superqt import QCollapsible
 
 
 class ProjectControl(QWidget):
@@ -24,9 +26,36 @@ class ProjectControl(QWidget):
             "background-color:rgb(38, 41, 48); border:0px"
         )
 
+        self.cache_images_check = QCheckBox()
+        self.orig_size_check = QCheckBox()
+        self.non_reg_image_check = QCheckBox()
+        self.write_merge_and_indiv_check = QCheckBox()
+        self.write_images_check = QCheckBox()
+
+        self.orig_size_check.setChecked(False)
+        self.non_reg_image_check.setChecked(True)
+        self.write_merge_and_indiv_check.setChecked(False)
+        self.cache_images_check.setChecked(True)
+        self.write_images_check.setChecked(True)
+
         proj_entry_layout.addRow("Set project name", self.project_name_entry)
         proj_entry_layout.addRow(self.output_dir_select, self.output_dir_entry)
+        proj_entry_layout.addRow("Write transformed images", self.write_images_check)
 
+        adv_opts = QCollapsible("Advanced project options")
+        adv_opts_widget = QWidget()
+        adv_opts_layout = QFormLayout()
+        adv_opts_layout.addRow("Cache prepro. images", self.cache_images_check)
+        adv_opts_layout.addRow(
+            "Write images to pre cropping size", self.orig_size_check
+        )
+        adv_opts_layout.addRow("Write non-transformed images", self.non_reg_image_check)
+        adv_opts_layout.addRow(
+            "Write merge and separate individual images",
+            self.write_merge_and_indiv_check,
+        )
+        adv_opts_widget.setLayout(adv_opts_layout)
+        adv_opts.addWidget(adv_opts_widget)
         action_layout = QHBoxLayout()
 
         self.write_config = QPushButton("Save config")
@@ -41,4 +70,5 @@ class ProjectControl(QWidget):
         action_layout.addWidget(self.run_reg)
 
         self.layout().addLayout(proj_entry_layout)
+        self.layout().addWidget(adv_opts)
         self.layout().addLayout(action_layout)
